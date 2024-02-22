a3e0011fcbcf4115aa789597821665502d4874e9
Горбулёв Сергей <gorbulevsv@yandex.ru>
2024-02-22 22:13:40 +0300

    1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <nav class="d-flex align-items-center bg-light position-fixed w-100 p-2" style="top:0">
 	<a class="btn btn-light text-dark bg-transparent border-0 fw-bold text-uppercase me-4" href="/"
-		>Привет</a
+		>Мой блог</a
 	>
 	<div>
 		<a class="btn btn-light text-dark" href="/">Главная</a>
